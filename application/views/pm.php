c84a34617a2467727b9cf7336eb2bbffc871c994
Adam2471 <adammubarok73@gmail.com>
2025-07-28 00:34:07 +0700

    navigasi footer ke litbang

--- application/views/pm.php
+++ application/views/pm.php
@@ -5,11 +5,8 @@
 				<div class="row g-5">
 					<div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
 						<h2 class="text-primary text-uppercase mb-2">Visi</h2>
-						<h5 class="mb-5">
-							"Mengembangkan dan Menciptakan Mahasiswa yang berjiwa pemimpin
-							semangat berorganisasi, Inovatif, Kreatif, Inspiratif, Kritis dan
-							ahli dalam bidang Teknik Informatika"
-						</h5>
+						<h5 class="mb-5">"Mengembangkan dan Menciptakan Mahasiswa yang berjiwa pemimpin semangat
+							berorganisasi, Inovatif, Kreatif, Inspiratif, Kritis dan ahli dalam bidang Teknik Informatika"</h5>
 						<!-- Misi -->
 						<div class="row gy-5 gx-4">
 							<h2 class="text-primary text-uppercase mb-2">Misi</h2>
@@ -18,10 +15,7 @@
 									<div class="flex-shrink-0 btn-square bg-primary me-3">
 										<i class="fa fa-check text-gradient"></i>
 									</div>
-									<p>
-										Meningkatkan Motivasi berorganisasi pada Mahasiswa Teknik
-										Informatika
-									</p>
+									<p>Meningkatkan Motivasi berorganisasi pada Mahasiswa Teknik Informatika</p>
 								</div>
 							</div>
 							<div class="col-sm-6 wow fadeIn" data-wow-delay="0.2s">
@@ -29,10 +23,7 @@
 									<div class="flex-shrink-0 btn-square bg-primary me-3">
 										<i class="fa fa-check text-white"></i>
 									</div>
-									<p class="mb-0">
-										Membuat sarana sharing ilmu dalam hal organisasi maupun
-										Akademik
-									</p>
+									<p class="mb-0">Membuat sarana sharing ilmu dalam hal organisasi maupun Akademik</p>
 								</div>
 							</div>
 							<div class="col-sm-6 wow fadeIn" data-wow-delay="0.3s">
@@ -40,70 +31,67 @@
 									<div class="flex-shrink-0 btn-square bg-primary me-3">
 										<i class="fa fa-check text-white"></i>
 									</div>
-									<p>
-										Membentuk kader-kader yang Berkualitas Tinggi pada prodi
-										Teknik Informatika
-									</p>
+									<p>Membentuk kader-kader yang Berkualitas Tinggi pada prodi Teknik Informatika</p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<!-- Kolom gambar logo -->
 					<div class="col-lg-6 wow fadeInUp d-flex justify-content-center align-items-center" data-wow-delay="0.5s">
-    					<div class="text-center">
-        					<img src="<?php base_url() ?>../assets/img/logo/him.png"
-             					alt="Logo HIMA"
-             					class="img-fluid"
-             					style="width: 80%; max-width: 500px; object-fit: contain;" />
-    					</div>
+						<div class="text-center">
+							<img src="<?php base_url() ?>../assets/img/logo/him.png"
+								alt="Logo HIMA"
+								class="img-fluid"
+								style="width: 80%; max-width: 500px; object-fit: contain;" />
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 		<!-- Features End -->
-		
-		<div class="container-xxl py-6">
-    	<div class="container">
-        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px">
-            <h4 class="display-6 mb-20">Tim Penjamin Mutu</h4>
-            <h6 class="text-primary text-uppercase mb-2"></h6>
-        </div>
-		<!-- Baris sejajar untuk dua orang -->
-		<div class="row justify-content-center mb-4">
-    	<!-- pm 1 -->
-    	<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="team-item position-relative">
-            <div class="position-relative">
-                <img class="img-fluid" src="<?php base_url() ?>../assets/img/pengurus/fiyah.jpg">
-                <div class="team-social text-center">
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-            <div class="bg-light text-center p-3">
-                <h5 class="mt-2">Lutfiyah Rahmah Azzahra</h5>
-                <span>Penjamin Mutu</span>
-            </div>
-        </div>
-    </div>
 
-    <!-- pm 2 -->
-    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
-        <div class="team-item position-relative">
-            <div class="position-relative">
-                <img class="img-fluid" src="<?php base_url() ?>../assets/img/pengurus/windi.jpg">
-                <div class="team-social text-center">
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-            <div class="bg-light text-center p-3">
-                <h5 class="mt-2">Ade Windi Anggraeni</h5>
-                <span>Penjamin Mutu</span>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+		<div class="container-xxl py-6">
+			<div class="container">
+				<div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px">
+					<h4 class="display-6 mb-20">Tim Penjamin Mutu</h4>
+					<h6 class="text-primary text-uppercase mb-2"></h6>
+				</div>
+				<!-- Baris sejajar untuk dua orang -->
+				<div class="row justify-content-center mb-4">
+					<!-- pm 1 -->
+					<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+						<div class="team-item position-relative">
+							<div class="position-relative">
+								<img class="img-fluid" src="<?php base_url() ?>../assets/img/pengurus/fiyah.jpg">
+								<div class="team-social text-center">
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-linkedin-in"></i></a>
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-instagram"></i></a>
+								</div>
+							</div>
+							<div class="bg-light text-center p-3">
+								<h5 class="mt-2">Lutfiyah Rahmah Azzahra</h5>
+								<span>Penjamin Mutu</span>
+							</div>
+						</div>
+					</div>
+
+					<!-- pm 2 -->
+					<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+						<div class="team-item position-relative">
+							<div class="position-relative">
+								<img class="img-fluid" src="<?php base_url() ?>../assets/img/pengurus/windi.jpg">
+								<div class="team-social text-center">
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-linkedin-in"></i></a>
+									<a class="btn btn-square btn-outline-primary border-2 m-1" href="#"><i class="fab fa-instagram"></i></a>
+								</div>
+							</div>
+							<div class="bg-light text-center p-3">
+								<h5 class="mt-2">Ade Windi Anggraeni</h5>
+								<span>Penjamin Mutu</span>
+							</div>
+						</div>
+					</div>
+				</div>
+	</body>
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,113 +1,104 @@
 <!-- Footer Start -->
 <div class="container-fluid bg-dark text-light footer my-6 mb-0 py-6 wow fadeIn"
-			data-wow-delay="0.1s">
-			<div class="container">
-				<div class="row g-7">
-					<div class="col-lg-4 col-md-6">
-						<h2 class="text-primary mb-4">
-							<i></i>HIMATIF
-						</h2>
-						<p class="mb-2">
-							<i class="fa fa-map-marker-alt me-3"></i>Jl. Inspeksi Kalimalang No.9, Cibatu, Cikarang Sel., Kabupaten Bekasi, Jawa Barat 17530
-						</p>
-						<p class="mb-2">
-							<i class="fa fa-phone-alt me-3"></i>089631149187
-						<p class="mb-2">
-							<i class="fa fa-envelope me-3"></i>himatif@pelitabangsa.ac.id
-						</p>
-					</div>
-					<div class="col-lg-4 col-md-8">
-						<h4 class="text-light mb-4">Quick Links</h4>
-						<a class="btn btn-link" href="">About Us</a>
-						<a class="btn btn-link" href="">Contact Us</a>
-						<a class="btn btn-link" href="">Our Services</a>
-						<a class="btn btn-link" href="">Terms & Condition</a>
-						<a class="btn btn-link" href="">Support</a>
-					</div>
-					
-					<div class="col-lg-3 col-md-6">
-						<!-- <h4 class="text-light mb-4">Newsletter</h4>-->
-						<!--<form action="">-->
-						<!--	<div class="input-group">-->
-						<!--		<input-->
-						<!--			type="text"-->
-						<!--			class="form-control p-3 border-0"-->
-						<!--			placeholder="Your Email Address"-->
-						<!--		/>-->
-						<!--		<button class="btn btn-primary">Sign Up</button>-->
-						<!--	</div>-->
-						<!--</form> -->
-						<h6 class="text-white mt-6 mb-3">Follow Us</h6>
-						<div class="d-flex pt-2">
-						
-							<a class="btn btn-square btn-outline-light me-1" href="">
-								<i class="fab fa-facebook-f"></i></a>
-							<a class="btn btn-square btn-outline-light me-1" href="https://www.youtube.com/channel/UC7SGrlmtAaBh2XTPpSO2Gfg">
-								<i class="fab fa-youtube"></i></a>
-							<a class="btn btn-square btn-outline-light me-0" href="https://www.instagram.com/himatif_pb/?hl=id">
-								<i class="fab fa-instagram"></i></a>
-						</div>
-					</br>
-						<div class="col-lg-4 col-md-2 h-800 w-200">
-							<a href="https://www.indowebsite.co.id/">
-								<img src="<?php base_url() ?>../assets/img/images/iw.png" alt="" >
-							</a>
-						</div>
-					</div>
+	data-wow-delay="0.1s">
+	<div class="container">
+		<div class="row g-7">
+			<div class="col-lg-4 col-md-6">
+				<h2 class="text-primary mb-4">
+					<i></i>HIMATIF
+				</h2>
+				<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>
+					Jl. Inspeksi Kalimalang No.9, Cibatu, Cikarang Sel., Kabupaten Bekasi, Jawa Barat 17530</p>
+				<p class="mb-2"><i class="fa fa-phone-alt me-3"></i>089631149187
+				<p class="mb-2"><i class="fa fa-envelope me-3"></i>himatif@pelitabangsa.ac.id</p>
+			</div>
+			<div class="col-lg-4 col-md-8">
+				<h4 class="text-light mb-4">Quick Links</h4>
+				<a class="btn btn-link" href="">About Us</a>
+				<a class="btn btn-link" href="">Contact Us</a>
+				<a class="btn btn-link" href="">Our Services</a>
+				<a class="btn btn-link" href="">Terms & Condition</a>
+				<a class="btn btn-link" href="">Support</a>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<!-- <h4 class="text-light mb-4">Newsletter</h4>-->
+				<!--<form action="">-->
+				<!--	<div class="input-group">-->
+				<!--		<input-->
+				<!--			type="text"-->
+				<!--			class="form-control p-3 border-0"-->
+				<!--			placeholder="Your Email Address"-->
+				<!--		/>-->
+				<!--		<button class="btn btn-primary">Sign Up</button>-->
+				<!--	</div>-->
+				<!--</form> -->
+				<h6 class="text-white mt-6 mb-3">Follow Us</h6>
+				<div class="d-flex pt-2">
+					<a class="btn btn-square btn-outline-light me-1" href=""><i class="fab fa-facebook-f"></i></a>
+					<a class="btn btn-square btn-outline-light me-1"
+						href="https://www.youtube.com/channel/UC7SGrlmtAaBh2XTPpSO2Gfg">
+						<i class="fab fa-youtube"></i></a>
+					<a class="btn btn-square btn-outline-light me-0"
+						href="https://www.instagram.com/himatif_pb/?hl=id">
+						<i class="fab fa-instagram"></i></a>
+				</div>
+				</br>
+				<div class="col-lg-4 col-md-2 h-800 w-200">
+					<a href="https://www.indowebsite.co.id/">
+						<img src="<?php base_url() ?>../assets/img/images/iw.png" alt="indowebsite"></a>
 				</div>
 			</div>
 		</div>
-		
-		<!-- Footer End -->
+	</div>
+</div>
+<!-- Footer End -->
 
-		<!-- Copyright Start -->
-		<div
-			class="copyright text-light py-0 wow fadeIn"
-			data-wow-delay="0.1s">
-			<div class="">
-				<div class="row">
-					<div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-						&copy; <a href="#">HIMATIF-Univ.Pelita Bangsa</a>
-					</div>
-					<div class="col-md-6 text-center text-md-end">
-						<i class="balloon-heart-fill"></i>
-						<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-						Designed By <a href="#">Departemen LITBANG</a>
-					</div>
-				</div>
+<!-- Copyright Start -->
+<div class="copyright text-light py-0 wow fadeIn" data-wow-delay="0.1s">
+	<div class="">
+		<div class="row">
+			<div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+				&copy; <a href="#">HIMATIF-Univ.Pelita Bangsa</a>
+			</div>
+			<div class="col-md-6 text-center text-md-end">
+				<i class="balloon-heart-fill">
+				</i>Designed By <a href="<?=base_url('Himatif/litbang')?>">Departemen LITBANG</a>
 			</div>
 		</div>
-		<!-- Copyright End -->
-		<!-- Back to Top -->
-		<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
-			<i class="bi bi-arrow-up"></i></a>
+	</div>
+</div>
+<!-- Copyright End -->
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
+	<i class="bi bi-arrow-up"></i></a>
 
-		<!-- Back to Top -->
-		<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
-			<i class="bi bi-arrow-up"></i></a>
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
+	<i class="bi bi-arrow-up"></i></a>
 
-		<!-- JavaScript Libraries -->
-		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-		<script src="<?php base_url() ?>../assets/lib/wow/wow.min.js"></script>
-		<script src="<?php base_url() ?>../assets/lib/easing/easing.min.js"></script>
-		<script src="<?php base_url() ?>../assets/lib/waypoints/waypoints.min.js"></script>
-		<script src="<?php base_url() ?>../assets/lib/owlcarousel/owl.carousel.min.js"></script>
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php base_url() ?>../assets/lib/wow/wow.min.js"></script>
+<script src="<?php base_url() ?>../assets/lib/easing/easing.min.js"></script>
+<script src="<?php base_url() ?>../assets/lib/waypoints/waypoints.min.js"></script>
+<script src="<?php base_url() ?>../assets/lib/owlcarousel/owl.carousel.min.js"></script>
 
-		<!-- Template Javascript -->
-		<script src="<?php base_url() ?>../assets/js/main.js"></script>
-		<script src="<?php base_url() ?>../assets/js/jquery.magnific-popup.min.js"></script>
-		<script src="<?php base_url() ?>../assets/js/royal_preloader.min.js"></script>
-		<script src="<?php base_url() ?>../assets/js/jquery.magnific-popup.min.js"></script>
-		<script src="<?php base_url() ?>../assets/js/jquery.isotope.min.js"></script>
-		<script src="<?php base_url() ?>../assets/js/easypiechart.js"></script>
-		<script src="<?php base_url() ?>../assets/js/owl.carousel.min.js"></script>
-		<script src="<?php base_url() ?>../assets/js/particles.min.js"></script>
-	</body>
+<!-- Template Javascript -->
+<script src="<?php base_url() ?>../assets/js/main.js"></script>
+<script src="<?php base_url() ?>../assets/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php base_url() ?>../assets/js/royal_preloader.min.js"></script>
+<script src="<?php base_url() ?>../assets/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php base_url() ?>../assets/js/jquery.isotope.min.js"></script>
+<script src="<?php base_url() ?>../assets/js/easypiechart.js"></script>
+<script src="<?php base_url() ?>../assets/js/owl.carousel.min.js"></script>
+<script src="<?php base_url() ?>../assets/js/particles.min.js"></script>
+</body>
+
 </html>
 <script type="text/javascript">
 	window.jQuery = window.$ = jQuery;
-	(function ($) {
+	(function($) {
 		"use strict";
 		//Preloader
 		Royal_Preloader.config({
